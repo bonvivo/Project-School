db9e064583bd5597dfaed63badb6d440a3d1f9ed
macolszewski <olschevski@gmail.com>
2019-01-19 15:34:46 +0100

    init project

--- src/main/java/project/school/SchoolApplication.java
+++ src/main/java/project/school/SchoolApplication.java
@@ -3,13 +3,14 @@ package project.school;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SchoolApplication {
 
-    public static void main(String[] args) {
-        System.out.println("Hello Wolrd!");
-        SpringApplication.run(SchoolApplication.class, args);
-    }
+	public static void main(String[] args) {
+
+		SpringApplication.run(SchoolApplication.class, args);
+	}
 
 }
 
